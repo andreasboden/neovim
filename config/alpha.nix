6e8737c49276b7af1f66583deeff1d5b29f940d6
Andreas <andreas@hellofuture.se>
2024-06-10 16:00:16 +0200

    updated alpha

--- config/alpha.nix
+++ config/alpha.nix
@@ -1,7 +1,133 @@
+let
+  header = [
+    "                                                                     "
+    "       ████ ██████           █████      ██                     "
+    "      ███████████             █████                             "
+    "      █████████ ███████████████████ ███   ███████████   "
+    "     █████████  ███    █████████████ █████ ██████████████   "
+    "    █████████ ██████████ █████████ █████ █████ ████ █████   "
+    "  ███████████ ███    ███ █████████ █████ █████ ████ █████  "
+    " ██████  █████████████████████ ████ █████ █████ ████ ██████ "
+    "                                                                       "
+  ];
+  header_bloody = [
+    " ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓"
+    " ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒"
+    "▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░"
+    "▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██ "
+    "▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒"
+    "░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░"
+    "░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░"
+    "   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░   "
+    "         ░    ░  ░    ░ ░        ░   ░         ░   "
+    "                                ░                  "
+  ];
+  button_opts = {
+    cursor = 3;
+    width = 50;
+    align_shortcut = "right";
+    hl_shortcut = "Keyword";
+    position = "center";
+  };
+  padding = val: {
+    type = "padding";
+    inherit val;
+  };
+in
 {
   plugins.alpha = {
     enable = true;
-    theme = "dashboard";
+    layout = [
+      (padding 2)
+      {
+        type = "text";
+        val = header_bloody;
+        opts = {
+          hl = "AlphaHeader";
+          position = "center";
+        };
+      }
+      (padding 2)
+      {
+        type = "button";
+        val = "  Find File";
+        on_press.__raw = "require('telescope.builtin').find_files";
+        opts = {
+          # hl = "comment";
+          keymap = [
+            "n"
+            "f"
+            ":Telescope find_files <CR>"
+            {
+              noremap = true;
+              silent = true;
+              nowait = true;
+            }
+          ];
+          shortcut = "f";
+        } // button_opts;
+      }
+      (padding 1)
+      {
+        type = "button";
+        val = "  New file";
+        on_press.__raw = "function() vim.cmd[[ene]] end";
+        opts = {
+          shortcut = "n";
+          keymap = [
+            "n"
+            "n"
+            ":ene <BAR> startinsert <CR>"
+            {
+              noremap = true;
+              silent = true;
+              nowait = true;
+            }
+          ];
+        } // button_opts;
+      }
+      (padding 1)
+      {
+        type = "button";
+        val = "󰈚  Recent Files";
+        on_press.__raw = "require('telescope.builtin').oldfiles";
+        opts = {
+          # hl = "comment";
+          keymap = [
+            "n"
+            "r"
+            ":Telescope oldfiles <CR>"
+            {
+              noremap = true;
+              silent = true;
+              nowait = true;
+            }
+          ];
+          shortcut = "r";
+        } // button_opts;
+      }
+      (padding 1)
+      {
+        type = "button";
+        val = "  Quit Neovim";
+        on_press.__raw = "function() vim.cmd[[qa]] end";
+        opts = {
+          # hl = "comment";
+          keymap = [
+            "n"
+            "q"
+            ":qa<CR>"
+            {
+              noremap = true;
+              silent = true;
+              nowait = true;
+            }
+          ];
+          shortcut = "q";
+
+        } // button_opts;
+      }
+    ];
   };
 
   plugins.which-key.registrations = {
