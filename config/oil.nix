6c5066180c6a01efa401e10a053c9630f0d54aa2
Andreas <andreas@hellofuture.se>
2024-07-10 18:09:27 +0200

    updated settings of oil.nvim

--- config/oil.nix
+++ config/oil.nix
@@ -1,6 +1,17 @@
 {
   plugins.oil = {
     enable = true;
+    settings = {
+      view_options.show_hidden = true;
+      view_options.natural_order = true;
+      view_options.is_always_hidden = ''
+        function (name, _)
+          return name == '..' or name == '.git'
+        end
+      '';
+      defaultFileExplorer = true;
+
+    };
   };
 
   plugins.which-key.registrations = {
