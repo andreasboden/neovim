528449ac0a7f43d2701f97ea1e16d1757be0553c
Andreas <andreas@hellofuture.se>
2024-09-11 21:11:15 +0200

    roc stuff

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,6 +1,9 @@
-{
+{ pkgs, config, ... }: {
   plugins.treesitter = {
     enable = true;
     nixGrammars = true;
+    grammarPackages = config.plugins.treesitter.package.passthru.allGrammars ++ [
+      config.plugins.treesitter.package.builtGrammars.roc
+    ];
   };
 }
