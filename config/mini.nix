debe32c4a324fa3b5299d62a7b339cb4ea7ecd00
Andreas <andreas@hellofuture.se>
2024-11-02 22:00:17 +0100

    replaced telescope find files and grep files with mini.pick

--- config/mini.nix
+++ config/mini.nix
@@ -29,10 +29,59 @@
         autoread = true;
         autowrite = true;
       };
+
+      pick = {
+        enable = true;
+      };
+
+
       files = {
         enable = true;
+        content = {
+          filter.__raw = ''
+            function(entry)
+              return entry.name ~= '.DS_Store' and entry.name ~= '.git' and entry.name ~= '.direnv' and entry.name ~= '.devenv' and entry.name ~= '.cache'
+            end
+          '';
+          sort.__raw = ''
+            function(entries)
+              -- technically can filter entries here too, and checking gitignore for _every entry individually_
+              -- like I would have to in `content.filter` above is too slow. Here we can give it _all_ the entries
+              -- at once, which is much more performant.
+              local all_paths = table.concat(
+                vim.tbl_map(function(entry)
+                  return entry.path
+                end, entries),
+                '\n'
+              )
+              local output_lines = {}
+              local job_id = vim.fn.jobstart({ 'git', 'check-ignore', '--stdin' }, {
+                stdout_buffered = true,
+                on_stdout = function(_, data)
+                  output_lines = data
+                end,
+              })
+
+              -- command failed to run
+              if job_id < 1 then
+                return entries
+              end
+
+              -- send paths via STDIN
+              vim.fn.chansend(job_id, all_paths)
+              vim.fn.chanclose(job_id, 'stdin')
+              vim.fn.jobwait({ job_id })
+              return require('mini.files').default_sort(vim.tbl_filter(function(entry)
+                return not vim.tbl_contains(output_lines, entry.path)
+              end, entries))
+            end
+          '';
+
+        };
         windows = {
           preview = true;
+          width_nofocus = 25;
+          width_preview = 50;
         };
         mappings = {
           close = "q";
@@ -59,6 +108,16 @@
       __unkeyed-1 = "<leader>e";
       __unkeyed-2 = "<cmd>:lua MiniFiles.open()<CR>";
       desc = "Explorer";
+    }
+    {
+      __unkeyed-1 = "<leader>f";
+      __unkeyed-2 = "<cmd>Pick files tool='git'<CR>";
+      desc = "Find files";
+    }
+    {
+      __unkeyed-1 = "<leader>F";
+      __unkeyed-2 = "<cmd>Pick grep_live tool='git'<CR>";
+      desc = "Find in files";
     }
   ];
 }
--- config/telescope.nix
+++ config/telescope.nix
@@ -20,16 +20,16 @@
   };
 
 
-  plugins.which-key.settings.spec = [
-    {
-      __unkeyed-1 = "<leader>f";
-      __unkeyed-2 = "<cmd>Telescope find_files<CR>";
-      desc = "Find files";
-    }
-    {
-      __unkeyed-1 = "<leader>F";
-      __unkeyed-2 = "<cmd>Telescope live_grep<CR>";
-      desc = "Find in files";
-    }
-  ];
+  # plugins.which-key.settings.spec = [
+  #   {
+  #     __unkeyed-1 = "<leader>f";
+  #     __unkeyed-2 = "<cmd>Telescope find_files<CR>";
+  #     desc = "Find files";
+  #   }
+  #   {
+  #     __unkeyed-1 = "<leader>F";
+  #     __unkeyed-2 = "<cmd>Telescope live_grep<CR>";
+  #     desc = "Find in files";
+  #   }
+  # ];
 }
